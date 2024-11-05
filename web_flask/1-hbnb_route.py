33fa5d365cf3eb4f1d9c223c02be33b8a53b939a
“Tristian <tristian.davis@atlasschool.com>
2024-11-05 01:13:17 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" init file for flask"""
+from flask import Flask
+
+""" create flask app """
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ return hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ return HBNB """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
