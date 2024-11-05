b83e9ad4e530caf93c0a499c61fab74a4730ae07
“Tristian <tristian.davis@atlasschool.com>
2024-11-05 01:30:50 +0000

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" init file for flask"""
+from flask import Flask
+
+""" create flask app """
+app = Flask(__name__)
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ return C and value of text """
+    return "C " + text.replace('_', ' ')
+    """ replace _ with space"""
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ return Python followed by text """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ return hello HBNB """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ return HBNB """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
