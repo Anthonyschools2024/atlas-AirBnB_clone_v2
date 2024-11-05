81b7728da331bfec886b07b2601f467b92eaf991
“Tristian <tristian.davis@atlasschool.com>
2024-11-05 19:04:27 +0000

    reverted, this is save point for working on task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,16 +3,20 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a HTML page with a list of states"""
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
-    return render_template('8-states_list.html', states=states)
+    return render_template('7-states_list.html', states=states)
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """Closes the storage on teardown"""
     storage.close()
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
