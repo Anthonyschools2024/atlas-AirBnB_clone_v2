e7176a74c1c77827db90ae7db65cf2ad969f6fce
“Tristian <tristian.davis@atlasschool.com>
2024-11-05 00:50:17 +0000

    hello route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+""" init file for flask"""
+from flask import Flask
+
+""" create flask app """
+app = Flask(__name__)
+
+""" route"""
+@app.route('/', strict_slashes=False)
+
+def hello_hbnb():
+    """ return hello HBNB """
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/__init__.py
+++
@@ -1,5 +0,0 @@
-""" init file for flask"""
-from flask import flask
-
-""" create flask app """
-app = Flask(__name__)
