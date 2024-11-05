c1ff690152bbeec3dd25fd382ab313a835d899ac
“Tristian <tristian.davis@atlasschool.com>
2024-11-05 00:54:23 +0000

    blank lines

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,12 @@ app = Flask(__name__)
 """ route"""
 @app.route('/', strict_slashes=False)
 
+
 def hello_hbnb():
     """ return hello HBNB """
+
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
