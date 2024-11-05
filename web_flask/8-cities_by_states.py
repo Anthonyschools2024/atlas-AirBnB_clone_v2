ab4f792bf16997d8cfec976d3d1b4ae65be07ff1
“Tristian <tristian.davis@atlasschool.com>
2024-11-05 22:24:40 +0000

    task 9 is partially working

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,7 +6,7 @@ from models.state import State
 
 app = Flask(__name__)
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """Displays a HTML page with a list of states"""
     states = storage.all(State).values()
