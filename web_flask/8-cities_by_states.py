b9bc8f4636b59d20d9c0c48cbd42c6ed3eb3bbf9
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-11-05 19:30:02 -0600

    Corrected output curl 0.0.0.0:5000/states_list

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,16 +6,20 @@ from models.state import State
 
 app = Flask(__name__)
 
-@app.route('/cities_by_states', strict_slashes=False)
+
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a HTML page with a list of states"""
+    """display list of states """
+    # retrieve data from storage
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
+    # render the 7-states_list.html file
+    return render_template('7-states_list.html', states=states)
+
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Closes the storage on teardown"""
+    """ closes the storage on teardown"""
     storage.close()
 
 if __name__ == "__main__":
